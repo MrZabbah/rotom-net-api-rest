ed9831b2779abde25e69396608ebca87eaa06eee
mrzabbah <56138357+MrZabbah@users.noreply.github.com>
2022-03-14 22:05:26 +0100

    Proyecto inicial para API REST de rotom net

---
+++ api-rotom-net/src/main/java/es/trident/apirotomnet/ApiRotomNetApplication.java
@@ -0,0 +1,14 @@
+package es.trident.apirotomnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+public class ApiRotomNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRotomNetApplication.class, args);
+	}
+
+}
---
+++ api-rotom-net/src/test/java/es/trident/apirotomnet/ApiRotomNetApplicationTests.java
@@ -0,0 +1,13 @@
+package es.trident.apirotomnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRotomNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
